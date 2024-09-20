5e25d764e4a73a81045afb247aad06a57cef297b
Amir Hossein <hosseinahmadvand2002@gmail.com>
2024-09-20 19:36:41 +0330

    complte Role usecase

---
+++ Shop/Shop.Application/Roles/Edit/EditRoleCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Common.Application;
+using Shop.Domain.RoleAgg;
+using Shop.Domain.RoleAgg.Repository;
+
+namespace Shop.Application.Roles.Edit
+{
+    public class EditRoleCommandHandler : IBaseCommandHandler<EditRoleCommand>
+    {
+        private readonly IRoleRepository _roleRepository;
+
+        public EditRoleCommandHandler(IRoleRepository roleRepository)
+        {
+            _roleRepository = roleRepository;
+        }
+
+        public async Task<OperationResult> Handle(EditRoleCommand request, CancellationToken cancellationToken)
+        {
+           var role=await _roleRepository.GetTracking(request.Id);
+            if (role == null)
+                return OperationResult.NotFound();
+
+            role.Edit(request.Title);
+
+            var permissions = new List<RolePermission>();
+            request.Permissions.ForEach(p =>
+            {
+                permissions.Add(new RolePermission(p));
+            });
+
+            role.SetPermissions(permissions);
+
+            await _roleRepository.Save();
+            return OperationResult.Success();
+        }
+    }
+}

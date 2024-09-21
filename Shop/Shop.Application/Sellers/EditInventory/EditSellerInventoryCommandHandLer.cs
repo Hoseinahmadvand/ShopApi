6d14a2420636bf9fed9b68bdc564f86791969ae0
Amir Hossein <hosseinahmadvand2002@gmail.com>
2024-09-21 14:23:30 +0330

    inventory seller

---
+++ Shop/Shop.Application/Sellers/EditInventory/EditSellerInventoryCommandHandLer.cs
@@ -0,0 +1,28 @@
+﻿using Common.Application;
+using Shop.Domain.SellerAgg.Repository;
+
+namespace Shop.Application.Sellers.EditInventory
+{
+    internal class EditSellerInventoryCommandHandLer : IBaseCommandHandler<EditSellerInventoryCommand>
+    {
+        private readonly ISellerRepository _repository;
+
+        public EditSellerInventoryCommandHandLer(ISellerRepository repository)
+        {
+            _repository = repository;
+         
+        }
+
+        public async Task<OperationResult> Handle(EditSellerInventoryCommand request, CancellationToken cancellationToken)
+        {
+            var inventory = await _repository.GetTracking(request.InventoryId);
+            if (inventory == null)
+                return OperationResult.NotFound();
+             inventory.EditInventory(request.InventoryId,request.Price,request.Count,request.DiscountPercentage);
+            await _repository.Save();
+            return OperationResult.Success();
+        }
+    }
+   
+
+}

41e1321a09c35753a345cf62854448dc1f112b7e
Amir Hossein <hosseinahmadvand2002@gmail.com>
2024-09-24 02:47:10 +0330

    conig layer

---
+++ Shop.Config/ShopBootstraper.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Application._Utilities;
+using Shop.Application.Categories;
+using Shop.Application.Products;
+using Shop.Application.Sellers;
+using Shop.Application.Users;
+using Shop.Domain.CategoryAgg.Service;
+using Shop.Domain.ProductAgg.Services;
+using Shop.Domain.SellerAgg.Services;
+using Shop.Domain.UserAgg.Services;
+using Shop.Infrastructure;
+using Shop.Query.Categories.GetById;
+
+namespace Shop.Config
+{
+    public static class ShopBootstraper
+    {
+        public static void Configure(this IServiceCollection services,string connectionString)
+        {
+          InfrastructureBootstrapper.Init(services, connectionString);
+
+            services.AddMediatR(typeof(Directories).Assembly);
+            services.AddMediatR(typeof(GetCategorByIdQuery).Assembly);
+
+            services.AddTransient<IProductDomainService, ProductDomainService>();
+            services.AddTransient<IUserDomainService, UserDomainService>();
+            services.AddTransient<ICategoryDomainService, CategoryDomainService>();
+            services.AddTransient<ISellerDomainService, SellerDomainService>();
+        }
+    }
+}
---
+++ Shop/Shop.Application/Categories/CategoryDomainService.cs
@@ -0,0 +1,16 @@
+﻿using Shop.Domain.CategoryAgg.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Categories;
+
+public class CategoryDomainService : ICategoryDomainService
+{
+    public bool IsSlugExist(string slug)
+    {
+        throw new NotImplementedException();
+    }
+}
